e60c7b15092117759cfc4f735167c8a5f96e51e0
Eric Luskin <el@cysic.xyz>
2026-03-30 16:24:33 +0000

    Fix proto path after JS compiler removal
    Point pil2-compiler-rust build.rs to the pilout.proto in pil2-proofman
    instead of the deleted pil2-compiler JS directory.

--- pil2-compiler-rust/build.rs
+++ pil2-compiler-rust/build.rs
@@ -1,11 +1,11 @@
 fn main() {
-    println!("cargo:rerun-if-changed=../pil2-compiler/src/pilout.proto");
+    println!("cargo:rerun-if-changed=../pil2-proofman/pilout/src/pilout.proto");
     let mut config = prost_build::Config::new();
     config.protoc_arg("--experimental_allow_proto3_optional");
     config
         .compile_protos(
-            &["../pil2-compiler/src/pilout.proto"],
-            &["../pil2-compiler/src/"],
+            &["../pil2-proofman/pilout/src/pilout.proto"],
+            &["../pil2-proofman/pilout/src/"],
         )
         .expect("Failed to compile pilout.proto");
 }
